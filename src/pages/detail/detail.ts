f70a4a822f795ea64e5fd7340aa8ccd91ff46841
Tim Ordenewitz <tim.ordenewitz@scandio.de>
2017-05-16 19:16:09 +0200

    Added proxy to be able to use Wikipedia Service without CORS, added wikipedia Service

--- src/pages/detail/detail.ts
+++ src/pages/detail/detail.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { Http } from '@angular/http';
+import { Http, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 /**
@@ -16,11 +16,27 @@ import 'rxjs/add/operator/map';
 })
 export class Detail {
   article: any;
-  posts: any;
+  wikipedia: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http) {
     this.article = navParams.get('article');
+    var authHeader = new Headers();
+    authHeader.append('Content-Type','jsonp');
+    this.getWikipediaData("Erdogan", authHeader);
     console.log(this.article);
+
+  }
+
+  private getWikipediaData(searchTerm, authHeader) {
+    this.http.get("http://localhost:8100/wikipedia?action=query&format=json&generator=search" +
+      "&gsrsearch="+ searchTerm +"&gsrnamespace=0&gsrlimit=10&prop=extracts|pageimages&exchars=200&exlimit=max&" +
+      "explaintext=true&exintro=true&piprop=thumbnail&pilimit=max&pithumbsize=200",{
+      headers: new Headers({
+        'Content-Type': 'jsonp'
+      })
+    }).map(res => res.json()).subscribe(data => {
+      this.wikipedia = data.query.pages;
+    });
   }
 
   ionViewDidLoad() {
