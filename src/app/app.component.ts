811d2a930bd639c661ecc58fb2e9bb2cb76c12d4
Oskar Hargedahl <oskarhargedahl@dhcp-10-176-97-230.dynamic.eduroam.mwn.de>
2017-05-31 10:08:10 +0200

    Implemented Facebook login, as well as a new tab called UserPage which collects data from FB-profile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,39 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, Nav} from 'ionic-angular';
+import { StatusBar, Splashscreen, NativeStorage } from 'ionic-native';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { ContentfulService } from './services/contentful.service'
 import { TabsPage } from '../pages/tabs/tabs';
+
+import { Login } from '../pages/login/login';
+import { UserPage } from '../pages/UserPage/UserPage';
 
 @Component({
   templateUrl: 'app.html',
   providers: [ContentfulService]
 })
 export class MyApp {
+  @ViewChild(Nav) nav: Nav;
   rootPage:any = TabsPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, splashScreen: SplashScreen) {
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
+      // Here we will check if the user is already logged in
+      // because we don't want to ask users to log in each time they open the app
+      let env = this;
+      NativeStorage.getItem('user')
+        .then( function (data) {
+          // user is previously logged and we have his data
+          // we will let him access the app
+          env.nav.push(UserPage);
+          Splashscreen.hide();
+        }, function (error) {
+          //we don't have the user data so we will ask him to log in
+          env.nav.push(Login);
+          Splashscreen.hide();
+        });
+
+      StatusBar.styleDefault();
     });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,21 +6,23 @@ import { MyApp } from './app.component';
 import { HttpModule } from '@angular/http';
 
 import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
+import { UserPage } from '../pages/UserPage/UserPage';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 import { Detail } from '../pages/detail/detail';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import {Login} from "../pages/login/login";
 
 @NgModule({
   declarations: [
     MyApp,
     AboutPage,
-    ContactPage,
+    UserPage,
     HomePage,
     TabsPage,
+    Login,
     Detail
   ],
   imports: [
@@ -32,9 +34,10 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     AboutPage,
-    ContactPage,
+    UserPage,
     HomePage,
     TabsPage,
+    Login,
     Detail
   ],
   providers: [
