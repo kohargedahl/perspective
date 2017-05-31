ac503a8e985772bb020bb762f1d9d74f5414e28c
Tim Ordenewitz <tim.ordenewitz@scandio.de>
2017-05-31 10:57:28 +0200

    Removed WikipediaPage Import.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,6 @@ import { UserPage } from '../pages/UserPage/UserPage';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 import { Detail } from '../pages/detail/detail';
-import { WikipediaPage } from '../pages/wikipedia/wikipedia';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
