ec48216617e792e9fa1cca0f53c4cb81a2c6f31e
Daniel Hartl <d_hartl@gmx.de>
2017-07-08 12:38:30 +0200

    - Implementation of Reddit to Detail Page - Solution for finding the right keyword out of the article which could be used for reddit

--- src/pages/details-reddit/details-reddit.ts
+++ src/pages/details-reddit/details-reddit.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 import { NavController, NavParams} from 'ionic-angular';
 
 @Component({
-  templateUrl: 'details.html'
+  templateUrl: 'src/pages/detail-reddit/details-reddit.html'
 })
-export class DetailsPage {
+export class DetailsReddit {
   item: any;
 
   constructor(public navCtrl: NavController, public params: NavParams) {
